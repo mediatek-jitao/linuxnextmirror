8e9f0934a07e699044d422ca9cfb553f25c72b41
Masami Hiramatsu <mhiramat@kernel.org>
2021-09-13 14:41:03 +0900

    bootconfig: Free copied bootconfig data after boot
    Free copied bootconfig data after booting kernel because that
    data will not be used anymore.
    
    commit 40caa127f3c7 ("init: bootconfig: Remove all bootconfig
    data when the init memory is removed") freed the bootconfig
    xbc_node array after booting kernel, but forgot to free the
    bootconfig data itself. This fixes that to free the bootconfig
    data too.
    
    This also frees the bootconfig data if the bootconfig data
    parsing failed.
    
    Link: https://lkml.kernel.org/r/163151166275.369741.12201304720604568345.stgit@devnote2
    
    Fixes: 40caa127f3c7 ("init: bootconfig: Remove all bootconfig data when the init memory is removed")
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- init/main.c
+++ init/main.c
@@ -319,6 +319,8 @@ static void * __init get_boot_config_from_initrd(u32 *_size, u32 *_csum)
 #ifdef CONFIG_BOOT_CONFIG
 
 static char xbc_namebuf[XBC_KEYLEN_MAX] __initdata;
+static void *init_xbc_data_copy __initdata;
+static phys_addr_t init_xbc_data_size __initdata;
 
 #define rest(dst, end) ((end) > (dst) ? (end) - (dst) : 0)
 
@@ -458,18 +460,24 @@ static void __init setup_boot_config(void)
 		else
 			pr_err("Failed to parse bootconfig: %s at %d.\n",
 				msg, pos);
+		memblock_free(__pa(copy), size + 1);
 	} else {
 		pr_info("Load bootconfig: %d bytes %d nodes\n", size, ret);
 		/* keys starting with "kernel." are passed via cmdline */
 		extra_command_line = xbc_make_cmdline("kernel");
 		/* Also, "init." keys are init arguments */
 		extra_init_args = xbc_make_cmdline("init");
+		init_xbc_data_copy = copy;
+		init_xbc_data_size = size + 1;
 	}
 	return;
 }
 
 static void __init exit_boot_config(void)
 {
+	if (!init_xbc_data_copy)
+		return;
+	memblock_free(__pa(init_xbc_data_copy), init_xbc_data_size);
 	xbc_destroy_all();
 }
 
