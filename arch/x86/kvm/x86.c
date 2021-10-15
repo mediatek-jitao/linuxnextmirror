c0f45f99b6952acc0cee9593fcde3f733593bc20
Paolo Bonzini <pbonzini@redhat.com>
2021-10-15 13:05:00 -0400

    KVM: X86: fix lazy allocation of rmaps
    If allocation of rmaps fails, but some of the pointers have already been written,
    those pointers can be cleaned up when the memslot is freed, or even reused later
    for another attempt at allocating the rmaps.  Therefore there is no need to
    WARN, as done for example in memslot_rmap_alloc, but the allocation *must* be
    skipped lest KVM will overwrite the previous pointer and will indeed leak memory.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -11392,7 +11392,8 @@ static int memslot_rmap_alloc(struct kvm_memory_slot *slot,
 		int level = i + 1;
 		int lpages = __kvm_mmu_slot_lpages(slot, npages, level);
 
-		WARN_ON(slot->arch.rmap[i]);
+		if (slot->arch.rmap[i])
+			continue;
 
 		slot->arch.rmap[i] = vcalloc(lpages, sz);
 		if (!slot->arch.rmap[i]) {
